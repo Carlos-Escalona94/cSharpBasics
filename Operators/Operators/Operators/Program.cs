c42c15aedbe979cbc955a597675e6d07e0cd4fe8
Carlos Escalona <ing.carlosuc@gmail.com>
2020-06-09 17:57:41 -0300

    Pratica operators

---
+++ Operators/Operators/Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 10;
+            var b = 3;
+            var c = 2;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a/b); //mostra treis (divisao enteira)
+            Console.WriteLine((float)a / (float)b);
+            Console.WriteLine(a + b * c); //mostra dezesseis
+            Console.WriteLine((a + b) * c); //mostra vinte e seis
+            Console.WriteLine(a > b); //mostra true
+            Console.WriteLine(a == b); //mostra false
+            Console.WriteLine(!(a != b)); //mostra false
+            Console.WriteLine(b > a && b > c); //mostra false
+            Console.WriteLine(b > a || b > c); //mostra true
+        }
+    }
+}
