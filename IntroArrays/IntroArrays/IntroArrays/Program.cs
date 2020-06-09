fe9fa383529daf0e57cf1fab37748d6aa826eb2f
Carlos Escalona <ing.carlosuc@gmail.com>
2020-06-09 18:57:47 -0300

    Pratica IntroArrays

---
+++ IntroArrays/IntroArrays/IntroArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntroArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var booleans = new bool[3];
+            booleans[0] = true;
+
+            Console.WriteLine(booleans[0]);
+            Console.WriteLine(booleans[1]);
+            Console.WriteLine(booleans[2]);
+
+            var names = new string[3] {"Jack", "John", "Mary"};
+        }
+    }
+}
