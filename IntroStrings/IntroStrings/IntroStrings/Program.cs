f3752ab4471a6297e0ee5b50a13587d60dd70cec
Carlos Escalona <ing.carlosuc@gmail.com>
2020-06-09 19:43:55 -0300

    Pratica IntroStrings

---
+++ IntroStrings/IntroStrings/IntroStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IntroStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firtsName = "Mosh";
+            String lastName = "Hamedani";
+            string myname = "Mosh";
+
+            var fullName = firtsName + " " + lastName;
+
+            var myFullName = string.Format("{0} {1}", firtsName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2";
+
+            Console.WriteLine(text);
+
+        }
+    }
+}
