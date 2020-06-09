91c314cae629af516ba6819ecbc5fa614d0a8316
Carlos Escalona <ing.carlosuc@gmail.com>
2020-06-09 16:59:19 -0300

    Praticas HelloWorld e Variables

---
+++ Variables/Variables/Variables/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const float Pi = 3.14f;
+            //Pi = 1.0f;  Pi is a constant can't be changed
+
+
+            /*
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.5f;
+            char character = 'A';
+            string firtsName = "Mosh";
+            bool isWorking = true;
+            
+
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.5f;
+            var character = 'A';
+            var firtsName = "Mosh";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firtsName);
+            Console.WriteLine(isWorking);
+            */
+
+
+            /*
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            */
+        }
+    }
+}
